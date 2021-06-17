7cb2b8525f84f8b761e698387c291180f9f567f7
kaylamuraoka <kmurs98@gmail.com>
2021-06-16 18:10:37 -1000

    checking api call

--- frontend/src/pages/LocationsPage.js
+++ frontend/src/pages/LocationsPage.js
@@ -1,10 +1,43 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Container } from "@material-ui/core";
+
+import axios from "axios";
 
 const LocationsPage = () => {
+  // Component State
+  const [locations, setLocations] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Side effects
+  useEffect(() => {
+    let cancel;
+
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const { data } = await axios({
+          method: "GET",
+          url: `/api/v1/locations`,
+          cancelToken: new axios.CancelToken((c) => (cancel = c)),
+        });
+
+        setLocations(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error.response.data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Locations Page</h1>
-    </div>
+    <Container>
+      {/* Filtering and Sorting Section */}
+
+      {/* Locations Listing */}
+    </Container>
   );
 };
 
