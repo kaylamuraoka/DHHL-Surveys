3d59a045941b47836576cdf15bb426b36c761619
kaylamuraoka <kmurs98@gmail.com>
2021-06-16 17:25:25 -1000

    adding new component page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import LocationPage from "./pages/LocationPage";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <h1>Hello World</h1>
       <Router>
         <Switch>
-          <Route exact path="/" component={} />
+          <Route exact path="/" component={LocationPage} />
         </Switch>
       </Router>
       {/* <header className="App-header">
