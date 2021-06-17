92d9ac623adca8103813c591b2faad55b1cadcfd
kaylamuraoka <kmurs98@gmail.com>
2021-06-17 12:12:00 -1000

    adding updates to locations page

--- frontend/src/components/LocationCard.js
+++ frontend/src/components/LocationCard.js
@@ -1,10 +1,41 @@
 import React from "react";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@material-ui/core";
 
 const LocationCard = ({ location }) => {
   return (
-    <div>
-      <p>{location._id}</p>
-    </div>
+    <Card>
+      <CardHeader
+        avatar={<Avatar />}
+        title={<Typography variant="h6">{location.objectId}</Typography>}
+      />
+
+      <CardContent>
+        <Typography variant="caption">{location.housePrfx}</Typography>
+        <Typography variant="caption">{location.houseNumbr}</Typography>
+        <Typography variant="caption">{location.houseSuffix}</Typography>
+        <Typography variant="caption">{location.streetName}</Typography>
+        <Typography variant="caption">{location.city}</Typography>
+        <Typography variant="caption">{location.zipCode}</Typography>
+        <Typography variant="h6" gutterBottom>
+          {location.subdivision}
+        </Typography>
+      </CardContent>
+
+      <CardActions>
+        <Button variant="contained" size="small" color="primary">
+          Start Survey
+        </Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
   );
 };
 
--- frontend/src/pages/LocationsPage.js
+++ frontend/src/pages/LocationsPage.js
@@ -4,7 +4,16 @@ import {
   Container,
   Grid,
   makeStyles,
+  Paper,
+  Typography,
+  Slider,
+  TextField,
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 import axios from "axios";
 import LocationCard from "../components/LocationCard";
@@ -19,15 +28,32 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",
   },
+  paper: {
+    marginBottom: "1rem",
+    padding: "13px",
+  },
+  filters: {
+    padding: "0 1.5rem",
+  },
+  idRangeInputs: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
 });
 
 const LocationsPage = () => {
   // Material UI Styles
   const classes = useStyles();
+  const history = useHistory();
 
   // Component State
   const [locations, setLocations] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const [sliderMax, setSliderMax] = useState(1000);
+  const [objectIdRange, setObjectIdRange] = useState([25, 75]);
+
+  const [filter, setFilter] = useState("");
 
   // Side effects
   useEffect(() => {
@@ -35,7 +61,6 @@ const LocationsPage = () => {
 
     const fetchData = async () => {
       setLoading(true);
-
       try {
         const { data } = await axios({
           method: "GET",
@@ -51,11 +76,83 @@ const LocationsPage = () => {
     };
 
     fetchData();
-  }, []);
+  }, [filter]);
+
+  const onSliderCommitHandler = (e, newValue) => {
+    buildRangeFilter(newValue);
+  };
+
+  const buildRangeFilter = (newValue) => {
+    const urlFilter = `?objectId[gte]=${newValue[0]}&objectId[lte]=${newValue[1]}`;
+
+    setFilter(urlFilter);
+
+    history.push(urlFilter);
+  };
 
   return (
     <Container className={classes.root}>
       {/* Filtering and Sorting Section */}
+      <Paper className={classes.paper}>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Typography gutterBottom>Filters</Typography>
+
+            <div className={classes.filters}>
+              <Slider
+                min={0}
+                max={sliderMax}
+                value={objectIdRange}
+                valueLabelDisplay="auto"
+                onChange={(e, newValue) => setObjectIdRange(newValue)}
+                onChangeCommitted={onSliderCommitHandler}
+              />
+
+              <div className={classes.idRangeInputs}>
+                <TextField
+                  size="small"
+                  id="lower"
+                  label="Min Object Id"
+                  variant="outlined"
+                  type="number"
+                  disabled={loading}
+                  value={0}
+                />
+
+                <TextField
+                  size="small"
+                  id="upper"
+                  label="Max Object Id"
+                  variant="outlined"
+                  type="number"
+                  disabled={loading}
+                  value={123}
+                />
+              </div>
+            </div>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Typography gutterBottom>Sort By</Typography>
+
+            <FormControl component="fieldset" className={classes.filters}>
+              <RadioGroup aria-label="objectId-order" name="objectId-order">
+                <FormControlLabel
+                  disabled={loading}
+                  control={<Radio />}
+                  label="Object ID: Highest - Lowest"
+                />
+
+                <FormControlLabel
+                  disabled={loading}
+                  control={<Radio />}
+                  label="Object ID: Lowest - Highest"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+        </Grid>
+      </Paper>
 
       {/* Locations Listing */}
       <Grid container spacing={2}>
